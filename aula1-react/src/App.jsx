a73e16393c596b310de5be10a2c707f767b5a13c
DAVID WANDERSON <profdavidwanderson@gmail.com>
2025-04-24 20:06:42 -0300

    commit inicial: projeto react + vite

---
+++ aula1-react/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React: Aula 1</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 10)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+
+
+// Navegador ler o arquivo index.html
+// O arquivo index.html é o ponto de entrada da aplicação
+// o arquivo main.jsx é o ponto de entrada do React
+// O arquivo App.jsx é o componente principal da aplicação
+// O arquivo App.css é o arquivo de estilos da aplicação
+
+// O arquivo vite.config.js é o arquivo de configuração do Vite
