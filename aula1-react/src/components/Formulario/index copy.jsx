7b9d49ea9b609011d5d9cb1adedc0c94bd3723b0
DAVID WANDERSON <profdavidwanderson@gmail.com>
2025-04-29 19:32:31 -0300

    alterações no componente de saudação

---
+++ aula1-react/src/components/Formulario/index copy.jsx
@@ -0,0 +1,14 @@
+import './form.css'
+
+function Formulario () {
+
+    return (
+        <div className="container">
+            <input type="text" placeholder="Nome" />
+            <input type="email" placeholder="E-mail" />
+            <button type="submit" onClick={() => alert('Enviado com sucesso!')}>Enviar</button>
+        </div>
+    )
+}
+
+export default Formulario
